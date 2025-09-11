b2386b78aca25109bc5b7b10d85c334926fd6e20
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-11 23:01:30 +0530

    Entity Class added

---
+++ src/main/java/com/questionservice/entity/Quiz.java
@@ -0,0 +1,28 @@
+package com.questionservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+public class Quiz
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String Category;
+
+    private int noOfQue;
+
+    @ManyToMany
+    @JoinTable(name = "quiz_questions_list")
+    private List<Questions> questionsList;
+
+}
