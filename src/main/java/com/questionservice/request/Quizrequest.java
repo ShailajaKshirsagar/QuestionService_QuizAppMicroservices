7b75cfb398e96969dbdacef056574d1b7d5761a6
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-11 23:02:20 +0530

    Quizrequest added

---
+++ src/main/java/com/questionservice/request/Quizrequest.java
@@ -0,0 +1,14 @@
+package com.questionservice.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Quizrequest
+{
+    private int queId;
+    
+    private int selectedOption;
+    
+}
